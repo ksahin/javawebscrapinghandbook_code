44c6d702abb567d7961055a88ad7647b72c513a7
ksahin <sahin.kevin@gmail.com>
2017-12-22 19:13:21 +0100

    Initial commit

---
+++ src/ebook_webscraping/HNScraper.java
@@ -0,0 +1,46 @@
+package ebook_webscraping;
+
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+public class HNScraper {
+
+	public static void main(String[] args) {
+		String baseUrl = "https://news.ycombinator.com/" ;
+		WebClient client = new WebClient();
+		client.getOptions().setCssEnabled(false);
+		client.getOptions().setJavaScriptEnabled(false);
+		try{
+			HtmlPage page = client.getPage(baseUrl);
+			List<HtmlElement> itemList =  page.getByXPath("//tr[@class='athing']");
+			if(itemList.isEmpty()){
+				System.out.println("No item found");
+			}else{
+				for(HtmlElement htmlItem : itemList){
+					int position = Integer.parseInt(((HtmlElement) htmlItem.getFirstByXPath("./td/span")).asText().replace(".", ""));
+					int id = Integer.parseInt(htmlItem.getAttribute("id"));
+					String title =  ((HtmlElement) htmlItem.getFirstByXPath("./td[not(@valign='top')][@class='title']")).asText();
+					String url = ((HtmlAnchor) htmlItem.getFirstByXPath("./td[not(@valign='top')][@class='title']/a")).getHrefAttribute();
+					String author =  ((HtmlElement) htmlItem.getFirstByXPath("./following-sibling::tr/td[@class='subtext']/a[@class='hnuser']")).asText();
+					int score = Integer.parseInt(((HtmlElement) htmlItem.getFirstByXPath("./following-sibling::tr/td[@class='subtext']/span[@class='score']")).asText().replace(" points", ""));
+					
+					HackerNewsItem hnItem = new HackerNewsItem(title, url, author, score, position, id);
+					
+					ObjectMapper mapper = new ObjectMapper();
+					String jsonString = mapper.writeValueAsString(hnItem) ;
+					
+					System.out.println(jsonString);
+				}
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			client.close();
+		}
+	}
+}
---
+++ src/ebook_webscraping/HackerNewsItem.java
@@ -0,0 +1,59 @@
+package ebook_webscraping;
+
+public class HackerNewsItem {
+	private String title;
+
+	private String url ;
+	private String author;
+	private int score;
+	private int position ;
+	private int id ;
+	
+	public HackerNewsItem(String title, String url, String author, int score, int position, int id) {
+		super();
+		this.title = title;
+		this.url = url;
+		this.author = author;
+		this.score = score;
+		this.position = position;
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getPosition() {
+		return position;
+	}
+	public void setPosition(int position) {
+		this.position = position;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+}
